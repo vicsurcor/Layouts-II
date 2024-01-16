420fbb6951a2cbb8a2daa0020d32e0e69e18f1e8
vicsurcor <vdsurcor@gmail.com>
2024-01-16 17:49:51 +0100

    Const_v1

--- app/src/main/java/com/example/layouts_ii_victorsuros/ConstraintLayout.kt
+++ app/src/main/java/com/example/layouts_ii_victorsuros/ConstraintLayout.kt
@@ -1,15 +1,23 @@
 package com.example.layouts_ii_victorsuros
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
 class ConstraintLayout : AppCompatActivity() {
+
+    private lateinit var botSiguiente: Button;
+    private lateinit var botVolver: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constraint_layout)
 
+        botSiguiente = findViewById(R.id.botonCambio)
+        botSiguiente.setOnClickListener {
 
+            setContentView(R.layout.constraint_layout_ii)
+        }
     }
 
 }
